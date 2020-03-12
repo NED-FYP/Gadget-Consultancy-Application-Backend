cde72fb35196cdaeb057d73026daa1cc00b69fe2
ned-fyp <ned.fyp1221@gmail.com>
2020-03-12 21:14:40 +0500

    Update gadgetconsultancy.js

--- routes/gadgetconsultancy.js
+++ routes/gadgetconsultancy.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const conn = require('../config/Database');
+var crypto = require('crypto');
+
+
 
                                /*USERS*/
 
 //Get all users
-router.get('/users',(req,res)=>{
+router.get('/signup/users',(req,res)=>{
     conn.query("SELECT * FROM users",(err,result)=>
     {
         if(err) throw err;
@@ -13,6 +16,49 @@ router.get('/users',(req,res)=>{
         console.log(result);
     });
 })
+//signing up
+
+router.post('/signup/users',(req,res)=>{
+    //const name =req.body.name;
+    const {user_name}=req.body
+    const {email_address}=req.body
+    const password =req.body.password
+    const hashedpassword = crypto.createHash('md5').update(password).digest('hex');
+    conn.query(`Insert into users(user_name,email_address, password ) Values ('${user_name}', '${email_address}', '${hashedpassword}')`
+    ,(err,result)=>
+    {
+        if(err) throw err;
+        res.json(result)
+        console.log(result);
+    });
+})
+//login in
+
+app.post('/login', (req, res) => {
+  let {email_address, password} = req.body;
+    User.findOne({email_address: email_address}, 'username email password', (err, userData) => {
+    	if (!err) {
+        	let passwordCheck = bcrypt.compareSync(password, userData.password);
+        	if (passwordCheck) { // we are using bcrypt to check the password hash from db against the supplied password by user
+                req.session.user = {
+                  email: userData.email,
+                  username: userData.username
+                  id: userData._id
+                }; // saving some user's data into user's session
+                req.session.user.expires = new Date(
+                  Date.now() + 3 * 24 * 3600 * 1000; // session expires in 3 days
+                );
+                res.status(200).send('You are logged in, Welcome!');
+            } else {
+            	res.status(401).send('incorrect password');
+            }
+        } else {
+        	res.status(401).send('invalid login credentials')
+        }
+    })
+})
+
+
 
 
 //Get all users by id
@@ -25,18 +71,7 @@ router.get('/heroes/:id',(req,res)=>{
       console.log(result[0]);
   });
 })
-//POST-Insert a user
-
-router.post('/heroes',(req,res)=>{
-    //const name =req.body.name;
-    const {name}=req.body
-    conn.query(`Insert into heroes(name) Values('${name}')`,(err,result)=>
-    {
-        if(err) throw err;
-        res.json(result)
-        console.log(result);
-    });
-})
+/
 
 //delete a user
 
@@ -64,3 +99,12 @@ router.put('/heroes/:id', (req, res) => {
   })
 
 module.exports=router;
+
+
+
+
+
+
+
+
+
