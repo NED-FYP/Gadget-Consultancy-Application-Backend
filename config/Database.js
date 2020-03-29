61f5b8a007d1feb17028705d438ce2899b85ed00
NED-FYP <ned.fyp1221@gmail.com>
2020-03-29 15:46:18 +0500

    Updated

--- config/Database.js
+++ config/Database.js
@@ -1,9 +1,9 @@
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database:'gadget consultancy'
+  host: "192.185.157.181",
+  user: "bazarist_gadget",
+  password: "gadget123@#",
+  database:'bazarist_gadget'
 });
 module.exports = con
