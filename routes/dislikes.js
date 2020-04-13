17837717ca87b12e053783e57014b00145486f1c
NED-FYP <ned.fyp1221@gmail.com>
2020-04-13 18:57:07 +0500

    Done with like & dislike api

--- routes/dislikes.js
+++ routes/dislikes.js
@@ -3,29 +3,30 @@ const router = express.Router();
 const conn = require('../config/Database');
 
 
-
 router.post('/dislikes',(req,res)=>{
-    var question_id=req.body.question_id
-    var user_id = req.body.user_id
-
-    conn.query( `select * from likes WHERE question_id = '${question_id}' and user_id = '${user_id}'` ,
-    function ( result) {
-        if(result.length > 0){
-          res.json({ msg: 'Already liked a question, Cannot dislike.' });
-        }else{
-				conn.query(  `Insert into dislikes(user_id, question_id) Values('${user_id}', '${question_id}')`
-				, function (err, result) {
-                    if (err){
-                        res.json({ msg: err.message });
-                    }
-              	});
-		
-			res.json(result)
-		}
-	});
-});
-
-
+  var question_id=req.body.question_id
+  var user_id = req.body.user_id
+  conn.query(`select * from likes WHERE question_id = '${question_id}' and user_id = '${user_id}'`
+  ,(err,result)=>
+  {
+          if (result.length > 0) 
+          {
+              if(result)
+              {
+                res.json('Already liked!! cannot dislike the question');
+              }
+            
+          } 
+          else 
+          {
+              conn.query(  `Insert into dislikes(user_id, question_id) Values('${user_id}', '${question_id}')`
+              , function (err, result) {
+                        if(err) throw err;
+                        res.json(result)
+              });
+          }			
+  });
+})
   
 
 router.delete('/dislikes/:id', (req, res) => {
--- routes/likes.js
+++ routes/likes.js
@@ -2,28 +2,55 @@ const express = require('express');
 const router = express.Router();
 const conn = require('../config/Database');
 
-
-
 router.post('/likes',(req,res)=>{
+  var question_id=req.body.question_id
+  var user_id = req.body.user_id
+  conn.query(`select * from dislikes WHERE question_id = '${question_id}' and user_id = '${user_id}'`
+  ,(err,result)=>
+  {
+          if (result.length > 0) 
+          {
+              if(result)
+              {
+                res.json('Already disliked!! cannot like the question');
+              }
+            
+          } 
+          else 
+          {
+              conn.query(  `Insert into likes(user_id, question_id) Values('${user_id}', '${question_id}')`
+              , function (err, result) {
+                        if(err) throw err;
+                        res.json(result)
+              });
+          }			
+  });
+})
+/** 
+router.get('/likes',(req,res)=>{
     var question_id=req.body.question_id
     var user_id = req.body.user_id
-
+    
     conn.query( `select * from dislikes WHERE question_id = '${question_id}' and user_id = '${user_id}'` ,
-    function ( result) {
-        if(result.length == ''){
+    function ( results , err ) { 
+        if( results != null ){
           res.json({ msg: 'Already disliked a question, Cannot like.' });
-        }else{
+        }
+        else{
 				conn.query(  `Insert into likes(user_id, question_id) Values('${user_id}', '${question_id}')`
 				, function (err, result) {
                     if (err){
                         res.json({ msg: err.message });
+                    res.json(result)  
                     }
               	});
 		
-			res.json(result)
+			//res.json(results)
 		}
-	});
-});
+  });
+
+});*/
+
 
 
   
