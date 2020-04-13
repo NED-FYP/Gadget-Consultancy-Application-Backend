cefd0338b9038068c781dc382419fff66102193a
NED-FYP <ned.fyp1221@gmail.com>
2020-04-14 02:40:27 +0500

    Done with both like & dislike api

--- routes/dislikes.js
+++ routes/dislikes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const conn = require('../config/Database');
 
 
-router.post('/dislikes',(req,res)=>{
+router.post('/likes',(req,res)=>{
   var question_id=req.body.question_id
   var user_id = req.body.user_id
   conn.query(`select * from likes WHERE question_id = '${question_id}' and user_id = '${user_id}'`
@@ -15,16 +15,33 @@ router.post('/dislikes',(req,res)=>{
               {
                 res.json('Already liked!! cannot dislike the question');
               }
-            
-          } 
+          }
           else 
           {
-              conn.query(  `Insert into dislikes(user_id, question_id) Values('${user_id}', '${question_id}')`
-              , function (err, result) {
-                        if(err) throw err;
-                        res.json(result)
-              });
+            conn.query( `select * from dislikes WHERE question_id = '${question_id}' and user_id = '${user_id}'`
+            , function (err, result) {
+                        if (result.length > 0) 
+                        {
+                            if(result)
+                            {
+                              conn.query( `Delete from dislikes WHERE question_id = '${question_id}' and user_id = '${user_id}'`
+                              , function (err, result) {
+                                        if(err) throw err;
+                                        res.json(result)
+                              });
+                            }
+                        }
+                       else{      
+                            conn.query(  `Insert into dislikes(user_id, question_id) Values('${user_id}', '${question_id}')`
+                            , function (err, result) {
+                                      if(err) throw err; 
+                                      res.json(result)    
+                            });
+                           }
+                     
+          });  
           }			
+        
   });
 })
   
