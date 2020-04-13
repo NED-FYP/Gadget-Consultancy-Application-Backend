e2869a831ae8ec7d16dac39143130f5593ca04b3
NED-FYP <ned.fyp1221@gmail.com>
2020-04-14 02:21:45 +0500

    Updated

--- routes/likes.js
+++ routes/likes.js
@@ -2,59 +2,6 @@ const express = require('express');
 const router = express.Router();
 const conn = require('../config/Database');
 
-router.post('/likes',(req,res)=>{
-  var question_id=req.body.question_id
-  var user_id = req.body.user_id
-  conn.query(`select * from dislikes WHERE question_id = '${question_id}' and user_id = '${user_id}'`
-  ,(err,result)=>
-  {
-          if (result.length > 0) 
-          {
-              if(result)
-              {
-                res.json('Already disliked!! cannot like the question');
-              }
-            
-          } 
-          else 
-          {
-              conn.query(  `Insert into likes(user_id, question_id) Values('${user_id}', '${question_id}')`
-              , function (err, result) {
-                        if(err) throw err;
-                        res.json(result)
-              });
-          }			
-  });
-})
-/** 
-router.get('/likes',(req,res)=>{
-    var question_id=req.body.question_id
-    var user_id = req.body.user_id
-    
-    conn.query( `select * from dislikes WHERE question_id = '${question_id}' and user_id = '${user_id}'` ,
-    function ( results , err ) { 
-        if( results != null ){
-          res.json({ msg: 'Already disliked a question, Cannot like.' });
-        }
-        else{
-				conn.query(  `Insert into likes(user_id, question_id) Values('${user_id}', '${question_id}')`
-				, function (err, result) {
-                    if (err){
-                        res.json({ msg: err.message });
-                    res.json(result)  
-                    }
-              	});
-		
-			//res.json(results)
-		}
-  });
-
-});*/
-
-
-
-  
-
 router.delete('/likes/:id', (req, res) => {
     let id = req.params.id
     conn.query(`DELETE FROM likes Where id=${id}`, function (err, result, fields) {
@@ -65,5 +12,48 @@ router.delete('/likes/:id', (req, res) => {
     });
   });
 
+  router.post('/likes',(req,res)=>{
+    var question_id=req.body.question_id
+    var user_id = req.body.user_id
+    conn.query(`select * from dislikes WHERE question_id = '${question_id}' and user_id = '${user_id}'`
+    ,(err,result)=>
+    {
+            if (result.length > 0) 
+            {
+                if(result)
+                {
+                  res.json('Already disliked!! cannot like the question');
+                }
+            }
+            else 
+            {
+              conn.query( `select * from likes WHERE question_id = '${question_id}' and user_id = '${user_id}'`
+              , function (err, result) {
+                          if (result.length > 0) 
+                          {
+                              if(result)
+                              {
+                                conn.query( `Delete from likes WHERE question_id = '${question_id}' and user_id = '${user_id}'`
+                                , function (err, result) {
+                                          if(err) throw err;
+                                          res.json(result)
+                                });
+                              }
+                          }
+                         else{      
+                              conn.query(  `Insert into likes(user_id, question_id) Values('${user_id}', '${question_id}')`
+                              , function (err, result) {
+                                        if(err) throw err; 
+                                        res.json(result)    
+                              });
+                             }
+                       
+            });  
+            }			
+          
+    });
+  })
+
+ 
   module.exports=router;
  
