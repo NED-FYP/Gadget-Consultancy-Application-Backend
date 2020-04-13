44774a5bba9cb259eb5c07aef19eb31f2b35948f
NED-FYP <ned.fyp1221@gmail.com>
2020-04-14 02:23:56 +0500

    Update likes.js

--- routes/likes.js
+++ routes/likes.js
@@ -14,9 +14,7 @@ router.post('/likes',(req,res)=>{
               {
                 res.json('Already disliked!! cannot like the question');
               }
-            
-          } 
-          else 
+            else
           {
               conn.query(  `Insert into likes(user_id, question_id) Values('${user_id}', '${question_id}')`
               , function (err, result) {
@@ -24,33 +22,9 @@ router.post('/likes',(req,res)=>{
                         res.json(result)
               });
           }			
+        }
   });
 })
-/** 
-router.get('/likes',(req,res)=>{
-    var question_id=req.body.question_id
-    var user_id = req.body.user_id
-    
-    conn.query( `select * from dislikes WHERE question_id = '${question_id}' and user_id = '${user_id}'` ,
-    function ( results , err ) { 
-        if( results != null ){
-          res.json({ msg: 'Already disliked a question, Cannot like.' });
-        }
-        else{
-				conn.query(  `Insert into likes(user_id, question_id) Values('${user_id}', '${question_id}')`
-				, function (err, result) {
-                    if (err){
-                        res.json({ msg: err.message });
-                    res.json(result)  
-                    }
-              	});
-		
-			//res.json(results)
-		}
-  });
-
-});*/
-
 
 
   
@@ -65,5 +39,45 @@ router.delete('/likes/:id', (req, res) => {
     });
   });
 
+
+
+  router.post('/likess',(req,res)=>{
+    var question_id=req.body.question_id
+    var user_id = req.body.user_id
+    conn.query(`select * from dislikes WHERE question_id = '${question_id}' and user_id = '${user_id}'`
+    ,(err,result)=>
+    {
+            if (result.length > 0) 
+            {
+                if(result)
+                {
+                  res.json('Already disliked!! cannot like the question');
+                }
+            }
+            else 
+            {
+              conn.query( `select * from likes WHERE question_id = '${question_id}' and user_id = '${user_id}'`
+              , function (err, result) {
+                          if (result.length > 0) 
+                          {
+                              if(result)
+                              {
+                                conn.query( `Delete from likes WHERE question_id = '${question_id}' and user_id = '${user_id}'`
+                                , function (err, result) {
+                                          if(err) throw err;
+                                });
+                              }
+                          }
+                       });
+                conn.query(  `Insert into likes(user_id, question_id) Values('${user_id}', '${question_id}')`
+                , function (err, result) {
+                          if(err) throw err;
+                });
+                res.json(result)
+            }			
+          
+    });
+  })               
+   
   module.exports=router;
  
