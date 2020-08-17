580d6c453e36924946e40b97aa99e215604ca096
NED-FYP <57455687+NED-FYP@users.noreply.github.com>
2020-08-18 00:58:06 +0500

    updated

--- config/Database.js
+++ config/Database.js
@@ -1,9 +1,18 @@
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database:'gadgetconsultancy'
+});
+module.exports = con
+
+/** 
+var con = mysql.createConnection({
   host: "192.185.157.181",
   user: "bazarist_gadget",
   password: "gadget123@#",
   database:'bazarist_gadget'
 });
-module.exports = con
+*/
